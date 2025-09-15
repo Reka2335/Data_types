c51a578806c567e2fd90727b4de059b4e73a1bd7
Reka2335 <aroslavdrapezo@gmail.com>
2025-09-15 20:03:05 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from .dicts import task_1_dict, task_2_dict, task_3_dict, task_4_dict, task_5_di
 from .lists import task_1_list, task_2_list, task_3_list, task_4_list, task_5_list
 from .sets import task_1_set, task_2_set, task_3_set, task_4_set, task_5_set
 from .tuples import task_1_tuple, task_2_tuple, task_3_tuple
+__all__ = ['task_1_list', 'task_2_list', 'task_3_tuple']
